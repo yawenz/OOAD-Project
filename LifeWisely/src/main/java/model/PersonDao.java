68e4513917515e36cdc97ea25f8d30538f867063
rshmshett <rash3706@colorado.edu>
2017-05-01 20:33:22 -0600

    Changes made for suspend user check

--- LifeWisely/src/main/java/model/PersonDao.java
+++ LifeWisely/src/main/java/model/PersonDao.java
@@ -6,6 +6,7 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 public class PersonDao  {
         private DataSource dataSource;
 
@@ -15,42 +16,31 @@ public class PersonDao  {
 
         public void create(String firstName, String lastName) {
                 JdbcTemplate insert = new JdbcTemplate(dataSource);
-                insert.update("INSERT INTO User (username, password) VALUES(?,?)",
+                insert.update("INSERT INTO user (username, password) VALUES(?,?)",
                                 new Object[] { firstName, lastName });
         }
         public boolean checkuser(String username, String password){
     		boolean userExists = false;
     		JdbcTemplate jdbcTemplate= new JdbcTemplate(dataSource);
-    		int rowcount = jdbcTemplate.queryForInt("select count(*) from User " +
-    				" where username = ? and password = ?",
+    		int rowcount = jdbcTemplate.queryForInt("select count(*) from user " +
+    				" where username = ? and password = ? and is_suspended = '0'",
     				username,password);
     		if(rowcount==1){
     			userExists = true;
     		}
-    		return userExists;       
+    		return userExists;      
         }
+        
         public boolean check_userexist(String username){
         	boolean userExists = false;
     		JdbcTemplate jdbcTemplate= new JdbcTemplate(dataSource);
     		int rowcount = jdbcTemplate.queryForInt("select count(*) from User " +
-    				" where username = ?",
+    				" where username = ? and is_suspended = '0'",
     				username);
     		if(rowcount==1){
     			userExists = true;
     		}
     		return userExists;
-        }
-        public int getUserReward(String username){
-        	JdbcTemplate jdbc=new JdbcTemplate(dataSource);
-        	int reward=0;
-        	reward=jdbc.queryForInt("select rewards from User where username=?", username);
-        	return reward;
-        }
-        public void updateUserReward(String username, int newReward){
-        	JdbcTemplate jdbc=new JdbcTemplate(dataSource);
-        	int reward=jdbc.queryForInt("select rewards from User where username=?", username);
-        	reward=reward+newReward;
-        	jdbc.update("update User set rewards=? where username=?", new Object[] {reward, username});
         }
             
 }
